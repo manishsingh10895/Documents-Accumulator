0fae557d8530705a37c49ac6e8817daca60c2c92
Joao Garin <joao.garin@gmail.com>
2016-03-26 19:59:11 +0100

    Remove counter

--- src/app/components/home.ts
+++ src/app/components/home.ts
@@ -7,26 +7,14 @@ import {Component,Inject} from 'angular2/core';
        <h1>{{name}}</h1>
        <input [(ngModel)]="name" />
     </div>
-    <br/>
-    <div>
-       <span>{{counter}}</span>
-       <button (click)="incrementCounter()">Increment</button>
-    </div>
     `
 })
 export class Home {
     name: string;
-    counter: number;
-
+    
     constructor(@Inject('AppStore') private appStore) {
         let state = this.appStore.getState();
         this.name = state.username;
-        this.counter = 0;
-    }
-
-    incrementCounter() {
-        let newCounter = this.counter + 1;
-        this.counter = newCounter;
     }
 
     ngOnInit() {
