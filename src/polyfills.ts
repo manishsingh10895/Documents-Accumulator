1a7807c8f1befc4fd11abe7e821f950562adb840
Joao Garin <joao.garin@gmail.com>
2016-03-30 10:08:29 +0200

    Go back to es6 shim and reflect isntead of core js (too big for this purpose)

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,12 +2,12 @@
 // (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
 
 // import 'ie-shim'; // Internet Explorer
-// import 'es6-shim';
-// import 'es6-promise';
-// import 'es7-reflect-metadata';
+ import 'es6-shim';
+ //import 'es6-promise';
+ import 'es7-reflect-metadata';
 
 // Prefer CoreJS over the polyfills above
-import 'core-js';
+//import 'core-js';
 require('zone.js/dist/zone.js');
 
 if ('production' === ENV) {
