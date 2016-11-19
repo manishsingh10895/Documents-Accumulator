263ceef4a789b6207584edc134d5eacd3f93e49d
manishsingh10895 <manishsingh10895@gmail.com>
2016-11-19 16:55:14 +0530

    Dev

--- src/app/main.js
+++ src/app/main.js
@@ -29,7 +29,7 @@ const createWindow = () => {
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Clear out the main window when the app is closed
   mainWindow.on('closed', () => {
